40e533f10b54eb5784a9b3bd404a7538c4424942
parsakhalaji <parsa.khalaji@gmail.com>
2024-04-07 23:28:02 +0330

    project init

---
+++ game.js
@@ -0,0 +1,286 @@
+var cvs = document.getElementById('mycanvas');
+var ctx = cvs.getContext('2d');
+
+
+var frames = 0;
+var DEGREE = Math.PI / 180;
+var sprite = new Image();
+sprite.src = 'img/sprite.png';
+
+var SCORE = new Audio();
+SCORE.src = 'audio/score.wav'
+
+var FLAP = new Audio();
+FLAP.src = 'audio/flap.wav'
+
+var DIE = new Audio();
+DIE.src = 'audio/die.wav'
+
+var START = new Audio();
+START.src = 'audio/start.wav'
+
+var HIT = new Audio();
+HIT.src = 'audio/hit.wav'
+
+var state = {
+    current: 0,
+    getReady: 0,
+    game: 1,
+    over: 2
+}
+
+function clickHandler(){
+    switch (state.current) {
+        case state.getReady:
+            START.play();
+            state.current = state.game;
+            break;
+        case state.game:
+            FLAP.play();
+            bird.flap();
+            break;
+        default:
+            pipes.position = [];
+            bird.rotation = 0;
+            bird.speed = 0;
+            state.current = state.getReady;
+            score.value = 0;
+            break;
+    }
+}
+
+document.addEventListener('click', clickHandler);
+document.addEventListener('keydown' , function(e){
+    if(e.which == 32){
+        clickHandler();
+    }
+})
+
+var bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+var fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+    dx: 2,
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+
+    },
+    update: function(){
+        if(state.current == state.game){
+            this.x = (this.x - this.dx) % (this.w/2);
+        }
+    }
+}
+
+var pipes = {
+    top: {
+        sX : 553,
+        sY : 0,
+    },
+    bottom: {
+        sX : 502,
+        sY : 0,
+    },
+    w: 53,
+    h: 400,
+    dx: 2,
+    gap: 120,
+    position: [],
+    maxYPos: -150,
+    draw: function(){
+        for(var i = 0; i < this.position.length; i++){
+            var p = this.position[i];
+            var topYpos = p.y;
+            var bottomYpos = p.y + this.h + this.gap;
+            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYpos, this.w, this.h);
+            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYpos, this.w, this.h);
+        }
+    },
+    update: function(){
+        if(state.current != state.game) return;
+        if(frames % 100 == 0){
+            this.position.push({
+                x: cvs.width ,
+                y: this.maxYPos * (Math.random() + 1) ,
+            })
+        };
+        for(var i = 0; i < this.position.length; i++){
+            var p = this.position[i];
+
+            var bottomPipesPos = p.y + this.h + this.gap;
+            
+            if( bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w
+                && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h ){
+                state.current = state.over;
+                HIT.play();
+            }
+            if( bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w 
+                && bird.y + bird.radius > bottomPipesPos && bird.y - bird.radius < bottomPipesPos + this.h ){
+                state.current = state.over;
+                HIT.play();
+            }
+
+            p.x -= this.dx;
+
+            if(p.x + this.w <= 0){
+                this.position.shift();
+                score.value++;
+                SCORE.play();
+                score.best = Math.max(score.value , score.best);
+                localStorage.setItem('best' , score.best);
+            }
+        };
+        
+    }
+}
+
+var getReady = {
+    sX: 0,
+    sY: 228,
+    w: 173,
+    h: 152,
+    x: cvs.width/2 - 173/2,
+    y: 80,
+    draw: function(){
+        if(state.current == state.getReady){
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+    }
+}
+
+var gameOver = {
+    sX: 175,
+    sY: 228,
+    w: 225,
+    h: 202,
+    x: cvs.width/2 - 225/2,
+    y: 90,
+    draw: function(){
+        if(state.current == state.over){
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+    }
+}
+
+var bird = {
+    animation : [
+        {sX: 276, sY:112},
+        {sX: 276, sY:139},
+        {sX: 276, sY:164},
+        {sX: 276, sY:139},
+    ],
+    w: 34,
+    h: 26,
+    x: 50,
+    y: 150,
+    animationIndex: 0,
+    speed: 0,
+    gravity: 0.25,
+    jump: 4.6,
+    rotation: 0,
+    radius: 12,
+    draw: function(){
+        var bird = this.animation[this.animationIndex];
+        ctx.save();
+        ctx.translate(this.x , this.y);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, - this.w/2, -this.h/2, this.w, this.h);
+        ctx.restore();
+    },
+    update: function(){
+        var period = state.current == state.getReady ? 10 : 5;
+        if(this.animationIndex >= 3) this.animationIndex = 0;
+        this.animationIndex += frames % period == 0 ? 1 : 0;
+        if(state.current == state.getReady){
+            this.y = 150;
+        }else{
+            this.speed += this.gravity;
+            this.y += this.speed;
+        }
+        if(this.y + this.h/2 >= cvs.height - fg.h){
+            this.y = cvs.height - fg.h - this.h/2;
+            this.animationIndex = 1;
+            if(state.current == state.game){
+                DIE.play();
+                state.current = state.over;
+            }
+        }
+        if(this.speed < this.jump){
+            this.rotation = DEGREE * -25;
+        }else{
+            this.rotation = DEGREE * 90;
+        }
+    },
+    flap: function(){
+        this.speed = - this.jump;
+    }
+}
+
+var score = {
+    best : parseInt(localStorage.getItem('best')) || 0,
+    value : 0,
+    draw : function(){
+
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'black';
+
+        if(state.current == state.game){
+            ctx.lineWidth = 2;
+            ctx.font = '35px IMPACT';
+            ctx.fillText( this.value , cvs.width/2 , 50 );
+            ctx.strokeText( this.value , cvs.width/2 , 50 );
+        }else if(state.current == state.over){
+            ctx.lineWidth = 2;
+            ctx.font = '25px IMPACT';
+            ctx.fillText( this.best , 225 , 230 );
+            ctx.strokeText( this.best , 225 , 230 );
+
+            ctx.fillText( this.value , 225 , 190 );
+            ctx.strokeText( this.value , 225 , 190 );
+        }
+    }
+}
+
+function update(){
+    bird.update();
+    fg.update();
+    pipes.update();
+}
+
+function draw(){
+    ctx.fillStyle = '#70c5ce'
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    bg.draw();
+    pipes.draw();
+    fg.draw();
+    bird.draw();
+    getReady.draw();
+    gameOver.draw();
+    score.draw();
+}
+
+function animate(){
+    update();
+    draw();
+    frames++;
+    requestAnimationFrame(animate);
+}
+
+animate();
